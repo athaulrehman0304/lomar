27a79faa0edff0045aff1f9939c2cada14c2d376
ATHAUL REHMAN <146646777+athaulrehman0304@users.noreply.github.com>
2026-02-23 00:32:27 +0530

    Refactor static and media file configurations
    Updated static and media file settings in Django.

--- lomar/settings.py
+++ lomar/settings.py
@@ -110,12 +110,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR,'Template/images')]
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'Template/media')
-
-STATIC_ROOT = '/static/'
-
-STATIC_URL = '/static/'
